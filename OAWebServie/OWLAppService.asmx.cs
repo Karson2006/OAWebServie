b732cf6c1f89d9d649668fd3e373a16cff7bf80a
wtc <382874559@qq.com>
2020-10-21 14:09:34 +0800

    实现业务招待费报销功能

--- OAWebServie/OWLAppService.asmx.cs
+++ OAWebServie/OWLAppService.asmx.cs
@@ -23,43 +23,6 @@ namespace TR.OAWebServie
         [WebMethod]
         public string GetEntertainmentExpensesList(string xmlMessage)
         {
-                string result =  "<GetList>" +
-                              "<Result>False</Result>" +
-                              "<Description></Description></GetList>";
-                string logID = Guid.NewGuid().ToString();
-                try
-                {
-                    FileLogger.WriteLog( logID + "|Start:" + xmlMessage, 1, "OAWebService", "GetEntertainmentExpensesList","DataService" );
-
-                    if (Common.CheckAuthCode("GetList", xmlMessage))
-                    {
-                        OWLBusHelper obj = new OWLBusHelper();
-                        result = obj.GetEntertainmentExpendList(xmlMessage);
-                    }
-                }
-                catch (Exception err)
-                {
-                     result ="<GetList>" +
-                              "<Result>False</Result>" +
-                              "<Description>"+err.Message+"</Description></GetList>";
-                }
-                FileLogger.WriteLog(logID + "|End:" + result, 1, "OAWebService", "GetEntertainmentExpensesList", "DataService");
-                return result;
-        }
-        [WebMethod]
-        public string GetEntertainmentExpensesListJson( string JsonMessage)
-        {
-            string xmlString = iTR.Lib.Common.Json2XML(JsonMessage, "GetList");
-            string result = GetEntertainmentExpensesList( xmlString);
-            result = iTR.Lib.Common.XML2Json(result, "GetList");
-            return result;
-        }
-
-        #endregion
-
-        [WebMethod]
-        public string SubmitEntertainmentExpensesForm(string xmlMessage)
-        {
             string result = "<GetList>" +
                           "<Result>False</Result>" +
                           "<Description></Description></GetList>";
@@ -84,5 +47,41 @@ namespace TR.OAWebServie
             return result;
         }
         [WebMethod]
+        public string GetEntertainmentExpensesListJson(string JsonMessage)
+        {
+            string xmlString = iTR.Lib.Common.Json2XML(JsonMessage, "GetList");
+            string result = GetEntertainmentExpensesList(xmlString);
+            result = iTR.Lib.Common.XML2Json(result, "GetList");
+            return result;
+        }
+
+        #endregion
+
+        [WebMethod]
+        public string SubmitEntertainmentExpensesForm(string xmlMessage)
+        {
+            string result = "<UpdateData>" +
+                          "<Result>False</Result>" +
+                          "<Description></Description></UpdateData>";
+            string logID = Guid.NewGuid().ToString();
+            try
+            {
+                FileLogger.WriteLog(logID + "|Start:" + xmlMessage, 1, "OAWebService", "SubmitEntertainmentExpendForm", "DataService");
+
+                if (Common.CheckAuthCode("UpdateData", xmlMessage))
+                {
+                    OWLBusHelper obj = new OWLBusHelper();
+                    result = obj.SubmitEntertainmentExpendForm(xmlMessage);
+                }
+            }
+            catch (Exception err)
+            {
+                result = "<UpdateData>" +
+                         "<Result>False</Result>" +
+                         "<Description>" + err.Message + "</Description></UpdateData>";
+            }
+            FileLogger.WriteLog(logID + "|End:" + result, 1, "OAWebService", "SubmitEntertainmentExpendForm", "DataService");
+            return result;
+        }
     }
 }
--- TR.OA.BusHelper/OWLBusHelper.cs
+++ TR.OA.BusHelper/OWLBusHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Xml;
 using iTR.Lib;
@@ -38,7 +39,7 @@ namespace TR.OA.BusHelper
         #region SubmitEntertainmentExpendForm
         public string SubmitEntertainmentExpendForm(string xmlString)
         {
-            string sql = "",, EmployeeeID = "", rowDatas = "",loginName= "", EmployeeeName = "";
+            string sql = "", EmployeeeID = "", rowDatas = "",loginName= "", EmployeeeName = "";
             string result = @"<UpdateData><Result>False</Result><Description></Description></UpdateData>";
             
             #region 参数定义
@@ -61,13 +62,14 @@ namespace TR.OA.BusHelper
             string field0031 = "";  //是否按计划
             string field0035 = "";  //医院
             string field0036 = "";  //可用额
+            string field0037 = "";  //在途额
             string field0045 = "";  //费用来源
             string field0050 = "";  //成本中心_归档
-           
+            string field0056 = "";  //招待费明细账已用额
             #endregion
 
             #region FormData
-            string formData = @"<formExport version=""2.0""""><summary id=""7810778149231805299"" name=""formmain_5935""/>
+            string formData = @"<formExport version=""2.0""><summary id=""7810778149231805299"" name=""formmain_5935""/>
                                             <definitions/>
                                             <values>
                                             <column name=""公司""><value>上海腾瑞制药有限公司</value></column>
@@ -95,11 +97,10 @@ namespace TR.OA.BusHelper
                                             <column name=""是否按计划""><value>{17}</value></column>
                                             <column name=""费用类型""><value>业务招待费</value></column>
                                             <column name=""医院""><value>{18}</value></column>
-                                            <column name=""可用额""><value>{19}</value></column>
-                                            <column name=""财务审批标志""><value>{20}</value></column>
-                                            <column name=""费用来源""><value>{21}</value></column>
-                                            <column name=""是否有发票""><value>0</value>
-                                            </column><column name=""成本中心_归档""><value>{4}</value></column>
+                                            <column name=""财务审批标志""><value>{19}</value></column>
+                                            <column name=""费用来源""><value>{20}</value></column>
+                                            <column name=""是否有发票""><value>0</value></column>
+                                            <column name=""成本中心_归档""><value>{4}</value></column>
                                             <column name=""YRB发起""><value>1</value></column>
                                             </values>
                                             <subForms>
@@ -112,14 +113,14 @@ namespace TR.OA.BusHelper
                                             <column id=""field0043"" type=""0"" name=""编码_分摊_0"" length=""255""/>
                                             <column id=""field0044"" type=""0"" name=""编码_分摊_1"" length=""255""/>
                                             </definitions>
-                                            <values>{22}</values>
+                                            <values>{21}</values>
                                             </subForm>
                                             </subForms>
                                             </formExport>";
 
             string rowData = @" <row>
                                                 <column name=""编码_分摊""><value>{0}</value></column>
-                                                <column name=""医院_分摊""><value>{1}/value></column>
+                                                <column name=""医院_分摊""><value>{1}</value></column>
                                                 <column name=""分摊金额""><value>{2}</value></column>
                                                 <column name=""分摊说明""><value>{3}</value></column>
                                                 <column name=""编码_分摊_0""><value>{4}</value>
@@ -138,13 +139,13 @@ namespace TR.OA.BusHelper
                 else
                     throw new Exception("申请人field0002节点不存在或为空");
 
-                sql = @"Select t1.ID As FEmployeeID, t1.Code As FEmployeeCode,t1.ORG_DEPARTMENT_ID AS FDeptID, Isnull(t2.field0008,'') As FCosstCenter,
-                            Isnull(t3.field0003,'') As FBank,Isnull(t3.field0005,'') As FAccount,Isnull(t3.field0004,'') As FCommpany,t4.LOGIN_NAME，t1.Name AS FEmployeeName
+                sql = @"Select t1.ID As FEmployeeID, t1.Code As FEmployeeCode,t1.ORG_DEPARTMENT_ID AS FDeptID, Isnull(t2.field0008,'') As FCostCenter,
+                            Isnull(t3.field0003,'') As FBank,Isnull(t3.field0005,'') As FAccount,Isnull(t3.field0004,'') As FCommpany,t4.LOGIN_NAME,t1.Name AS FEmployeeName
                             From  v3x.dbo.ORG_MEMBER t1
                             Left Join v3x.dbo.formmain_5499 t2 On t1.ORG_DEPARTMENT_ID= t2.field0002
-                            Left Join v3x.dbo.formmain_8130 t3 On Cast(t1.ID As varchar(25))= t2.field0001
+                            Left Join v3x.dbo.formmain_8130 t3 On t3.field0002= t1.Code
                             Left Join v3x.dbo.ORG_PRINCIPAL t4 On t1.ID= t4.MEMBER_ID 
-                            Where  t1.ID={0} ";
+                            Where  t1.ID='{0}' ";
                 sql = string.Format(sql, EmployeeeID);
                 runner = new SQLServerHelper();
                 DataTable dt = runner.ExecuteSql(sql);
@@ -167,7 +168,8 @@ namespace TR.OA.BusHelper
                 else
                     throw new Exception("单号不能为空");
                 //读取招待费明细表可用额
-                sql = @"Select t1.field0006 As field0045,t2.SHOWVALUE AS field0045_Name,t1.field0009 As field0036,t3.field0011 As field0017
+                sql = @"Select t1.field0006 As field0045,t2.SHOWVALUE AS field0045_Name,Isnull(t1.field0009,0) As field0036,Isnull(t3.field0011,0) As field0017,
+                            Isnull(t1.field0007,0) As field0056,t1.field0018 AS field0025,Isnull(t1.field0008,0) AS field0037
                             from v3x.dbo.formmain_6185 t1
                             Left Join v3x.dbo.CTP_ENUM_ITEM  t2 On t1.field0006= t2.ID
                             Left Join v3x.dbo.formmain_6180  t3 On t1.field0018= t3.field0001
@@ -178,7 +180,14 @@ namespace TR.OA.BusHelper
                     field0045 = dt.Rows[0]["field0045"].ToString();
                     field0036 = dt.Rows[0]["field0036"].ToString();
                     field0017 = dt.Rows[0]["field0017"].ToString();
+                    field0056 = dt.Rows[0]["field0056"].ToString();
+                    field0025 = dt.Rows[0]["field0025"].ToString();
+                    field0037 = dt.Rows[0]["field0037"].ToString();
                 }
+                //不能分次核销,在途额 +  可用额 
+                if (decimal.Parse(field0037) + decimal.Parse(field0056) > 0)
+                    throw new Exception("招待费不能分次核销");
+
                 //是否按计划
                 node = doc.SelectSingleNode("UpdateData/field0031");
                 if (node != null)
@@ -214,16 +223,31 @@ namespace TR.OA.BusHelper
                 node = doc.SelectSingleNode("UpdateData/field0023");
                 if (node != null)
                     field0023 = node.InnerText.Trim();
+                else
+                    throw new Exception("申请金额不能为空");
+                if (field0023.Trim().Length == 0)
+                    throw new Exception("申请金额不能为空");
 
                 //报销金额
                 node = doc.SelectSingleNode("UpdateData/field0024");
                 if (node != null)
                     field0024 = node.InnerText.Trim();
+                else
+                    throw new Exception("报销金额不能为空");
 
-                //费用申请单号
-                node = doc.SelectSingleNode("UpdateData/field0025");
-                if (node != null)
-                    field0025 = node.InnerText.Trim();
+                if (field0024.Trim().Length == 0)
+                    throw new Exception("报销金额不能为空");
+
+                if (decimal.Parse(field0023) < decimal.Parse(field0024))
+                    throw new Exception("报销金额不能大于申请金额");
+
+                if (decimal.Parse(field0017) < decimal.Parse(field0024))
+                    throw new Exception("报销金额不能大于月度可用额");
+
+                ////费用申请单号
+                //node = doc.SelectSingleNode("UpdateData/field0025");
+                //if (node != null)
+                //    field0025 = node.InnerText.Trim();
 
                 //医院
                 node = doc.SelectSingleNode("UpdateData/field0035");
@@ -231,18 +255,42 @@ namespace TR.OA.BusHelper
                     field0035 = node.InnerText.Trim();
 
                 XmlNodeList nodes = doc.SelectNodes("UpdateData/DataRows/DataRow");
+                //分摊医院不能为空
+                if (nodes.Count == 0)
+                    throw new Exception("分摊医院节点不能为空");
+
+                Dictionary<string, string> hospitalList = new Dictionary<string, string>();
+                string field0039 = "";
+
                 foreach (XmlNode row in nodes)
                 {
-                    rowDatas = rowDatas + string.Format(rowData, row["field0039"].InnerText, row["field0040"].InnerText, row["field0041"].InnerText,
-                        row["field0042"].InnerText, row["field0043"].InnerText, row["field0044"].InnerText);
-                }
-                formData = string.Format(formData, EmployeeeID, field0003, field0024, field0006, field0010, field0011, field0012, field0013, field0017, field0018, field0019,
-                    field0020, field0021, field0022, field0023, field0025, field0031, field0035, field0036, -1, field0045, rowDatas);
+                    if (row["field0043"].InnerText.Trim().Length > 0)
+                       field0039 = row["field0043"].InnerText.Trim();
+                    else
+                       field0039 = row["field0044"].InnerText.Trim();
+                    if (!hospitalList.ContainsKey(field0039))
+                        hospitalList.Add(field0039, row["field0040"].InnerText);
 
-                sql = @"Insert Into DataSevice.dbo.OATask([FTemplateCode],[FSenderLoginName],[FEmployeeCode],[FEmployeeName],[FSubject],[FData])
-	                     Values('OWL_HX_2020_01','{0}','{1}','{2}','{3}' , '{4}')";
-                sql = string.Format(sql, loginName, field0010, EmployeeeName, formData);
+                    rowDatas = rowDatas + string.Format(rowData, field0039, row["field0040"].InnerText, row["field0041"].InnerText,
+                        row["field0042"].InnerText, row["field0043"].InnerText, row["field0044"].InnerText);
+
+                }
+                if (hospitalList.ContainsKey("OA_19888"))
+                    throw new Exception("费用不能分摊到【跨区跨院活动】，请选择具体医院");
+                
+                if(nodes.Count > hospitalList.Count  )
+                    throw new Exception("分摊医院不能重复");
+
+                formData = string.Format(formData, EmployeeeID, field0003, field0024, field0006, field0007,field0010, field0011, field0012, field0013, field0017, field0018, field0019,
+                    field0020, field0021, field0022, field0023, field0025, field0031, field0035,  -1, field0045, rowDatas);
+
+                //doc.LoadXml(formData)
+
+                sql = @"Insert Into DataService.dbo.OATask([FTemplateCode],[FSenderLoginName],[FEmployeeCode],[FEmployeeName],[FSubject],[FData])
+	                     Values('O202001','{0}','{1}','{2}','{3}' , '{4}')";
+                sql = string.Format(sql, loginName, field0010, EmployeeeName,"招待费报销-YRB-"+ EmployeeeName+ "-"+ DateTime.Now.ToString(), formData);
                 runner.ExecuteSqlNone(sql);
+               
                 result = "<UpdateData><Result>True</Result><Description></Description></UpdateData>";
             }
             catch (Exception err)
